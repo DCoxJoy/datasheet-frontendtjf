9569e4ec58296cfb1187df5c713ed6b85fc89c80
BlueDeveloper-source <webstudio@payanimedia.com>
2025-02-13 13:45:20 +0530

    new build

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -636,92 +636,99 @@ class App extends Component {
     /*
         API fetching
     */
-    function getProductAndSiteInfo(params) {
-      const storeUrl = new URL(params.store_url);
-
-      // Use the store's canonical URL which should always resolve
-      const graphQLUrl = `${storeUrl.origin}/graphql`;
-
-      // Set up GraphQL query
-      // If specific product IDs were supplied, fetch them, else just get the first few products
-      const graphQLQuery = `
-        query SingleProduct {
-          site {
-             product(entityId: ${params.id}) {
-              entityId
-              brand {
-                name
-              }
-              name
-              sku
-              path
-              description
-              height {
-                value
-                unit
-              }
-              width {
-                value
-                unit
-              }
-              weight {
-                value
-                unit
-              }
-              depth {
-                value
-                unit
-              }
-              warranty
-              customFields {
-                edges {
-                  node {
-                    name
+        function getProductAndSiteInfo(params) {
+          const storeUrl = new URL(params.store_url);
+          const graphQLUrl = `${storeUrl.origin}/graphql`;
+          let data = JSON.stringify({
+            query: `query SingleProduct {
+                    site {
+                       product(entityId: ${params.id}) {
+                        entityId
+                        brand {
+                          name
+                        }
+                        name
+                        sku
+                        path
+                        description
+                        height {
+                          value
+                          unit
+                        }
+                        width {
+                          value
+                          unit
+                        }
+                        weight {
+                          value
+                          unit
+                        }
+                        depth {
+                          value
+                          unit
+                        }
+                        warranty
+                        customFields {
+                          edges {
+                            node {
+                              name
+                              value
+                            }
+                          }
+                        }
+                        defaultImage {
+                          img320px: url(width: 320)
+                          img640px: url(width: 640)
+                          img960px: url(width: 960)
+                        }
+                        images {
+                          edges {
+                            node {
+                              img320px:url(width:320)
+                            }
+                          }
+                        }
+                        prices {
+                          price {
+                            ...PriceFields
+                          }
+                        }
+                      }
+                    }
+                  }
+                    
+                  fragment PriceFields on Money {
                     value
-                  }
-                }
-              }
-              defaultImage {
-                img320px: url(width: 320)
-                img640px: url(width: 640)
-                img960px: url(width: 960)
-              }
-              images {
-                edges {
-                  node {
-                    img320px:url(width:320)
-                  }
-                }
-              }
-              prices {
-                price {
-                  ...PriceFields
-                }
-              }
-            }
-          }
+                    currencyCode
+                  }`,
+            variables: {},
+          });
+    
+          let config = {
+            method: "post",
+            maxBodyLength: Infinity,
+            url: graphQLUrl,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjaWQiOjEsImNvcnMiOlsiaHR0cDovL2xvY2FsaG9zdDozMDAwIl0sImVhdCI6MjE0NjAwMjU3OCwiaWF0IjoxNjUxNTc1NDM1LCJpc3MiOiJCQyIsInNpZCI6MTAwMDY3NjI0MCwic3ViIjoicXNzNHVsazZnZTYyNTRybWl2YWU3amJodjZ4bDBqeCIsInN1Yl90eXBlIjoyLCJ0b2tlbl90eXBlIjoxfQ.2LxDqgnyUNqZEYxDaLz-uffUczn6N2Rz2w1mmIZl_SkV9Fc6Uhxcpiu74EzO2uQCLP1y-sf6j7h3RaRGNX0z7Q",
+              Cookie:
+                "SF-CSRF-TOKEN=04ea22a9-552a-41b0-b07c-c9626a84601c; SHOP_SESSION_TOKEN=f6269edc-e2ba-47d2-a576-7f9bc95e9e5e; __cf_bm=umnodATYMi.2sege.Pw2GMfboyXiU3aHGuzWm4RN.IQ-1739369602-1.0.1.1-a8qodWKZKdahGbzfqXUfDp.sokzuLN26QNxUnM6yU92arrSE83ucHeRMoBAExXklWETg2bCCURNvj1ZSKfqE7g; athena_short_visit_id=bf9a3ea5-a9e1-4c8c-b637-655edaca65c7:1739369623; fornax_anonymousId=b1c357e8-d3b6-4a6d-9220-71d9367146a8",
+            },
+            data: data,
+          };
+    
+          return axios
+            .request(config)
+            .then((response) => {
+              console.log(JSON.stringify(response.data));
+              return response.data;
+            })
+            .catch((error) => {
+              console.error("GraphQL API Error:", error);
+              throw error;
+            });
         }
-          
-        fragment PriceFields on Money {
-          value
-          currencyCode
-        }`;
-
-      // Fetch data from the GraphQL Storefront API
-      return fetch(graphQLUrl, {
-        method: "POST",
-        credentials: "include",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${params.token}`,
-        },
-        Accept: "application/json",
-        body: JSON.stringify({ query: graphQLQuery }),
-      })
-        .then((res) => res.json())
-        .then((res) => res.data);
-    }
 
     // Set up default params (token expires 1/15/2038)hh
     let params = {
